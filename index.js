bcc58408f488366bc84a721a6e93dffdc91c1900
Andrew Oldham <otherworldlyguardian@gmail.com>
2017-02-25 20:21:47 +0000

    Done.

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
